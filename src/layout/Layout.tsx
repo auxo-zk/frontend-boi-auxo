d6147f8e87a7fa457e66d959fc6cf49a5d5a72b6
Le Dinh Quyen <quyenld9699@gmail.com>
2024-01-13 01:00:46 +0700

    feat: add thumbnail and metadata

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,24 +1,40 @@
-import React, { ReactNode } from "react";
-import { Provider as JotaiProvider } from "jotai";
-import { FontInter } from "src/assets/fonts";
-import { Box } from "@mui/material";
-import Head from "next/head";
-import InitStateAll from "src/states";
-import ThemeProviderCustom from "src/components/ThemeProviderCustom/ThemeProviderCustom";
-import ToastNotifier from "src/components/ToastNotifier/ToastNotifier";
-import ModalCustom from "src/components/ModalCustom/ModalCustom";
-import Sidebar from "./Sidebar/Sidebar";
-import Header from "./Header/Header";
+import React, { ReactNode } from 'react';
+import { Provider as JotaiProvider } from 'jotai';
+import { FontInter } from 'src/assets/fonts';
+import { Box } from '@mui/material';
+import Head from 'next/head';
+import InitStateAll from 'src/states';
+import ThemeProviderCustom from 'src/components/ThemeProviderCustom/ThemeProviderCustom';
+import ToastNotifier from 'src/components/ToastNotifier/ToastNotifier';
+import ModalCustom from 'src/components/ModalCustom/ModalCustom';
+import Sidebar from './Sidebar/Sidebar';
+import Header from './Header/Header';
 
 export default function Layout({ children }: { children: ReactNode }) {
-    const sidebarWidth = "202px";
-    const headerHeight = "64px";
+    const sidebarWidth = '202px';
+    const headerHeight = '64px';
     return (
         <>
             <Head>
                 <title>Auxo App</title>
                 <meta name="description" content="auxo app" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="language" content="English" />
+                <meta property="og:site_name" content="Auxo App" />
+                <meta property="og:image" content="/thumbnail.png" />
+                <meta property="og:image:type" content="image/png" />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
+                <meta name="twitter:image" content="/thumbnail.png" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large" />
+                <meta name="revisit-after" content="0 days" />
+                <meta name="ROBOTS" content="index, follow, max-snippet:-1, max-image-preview:large" />
+                <meta name="googlebot" content="index,follow" />
+                <meta name="BingBOT" content="index,follow" />
+                <meta name="yahooBOT" content="index,follow" />
+                <meta name="slurp" content="index,follow" />
+                <meta name="msnbot" content="index,follow" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <JotaiProvider>
@@ -29,17 +45,17 @@ export default function Layout({ children }: { children: ReactNode }) {
                 `}</style>
                 <InitStateAll />
                 <ThemeProviderCustom>
-                    <Box sx={{ position: "relative" }}>
+                    <Box sx={{ position: 'relative' }}>
                         <Sidebar sidebarWidth={sidebarWidth} headerHeight={headerHeight} />
                         <Box
                             sx={{
-                                position: "relative",
+                                position: 'relative',
                                 zIndex: 1,
                                 ml: { xs: 0, md: sidebarWidth },
                                 backgroundImage: `url(/images/bgheader1.png)`,
-                                backgroundRepeat: "no-repeat",
-                                backgroundPosition: "top center",
-                                backgroundSize: "975px auto",
+                                backgroundRepeat: 'no-repeat',
+                                backgroundPosition: 'top center',
+                                backgroundSize: '975px auto',
                             }}
                         >
                             <Header headerHeight={headerHeight}></Header>
