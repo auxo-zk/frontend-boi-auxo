0fe9bdc9ceb567974f9c5f3de8a4c7b208086813
Le Dinh Quyen <quyenld9699@gmail.com>
2024-07-12 14:26:11 +0700

    update data

--- src/services/baseUrl.ts
+++ src/services/baseUrl.ts
@@ -1,2 +1,2 @@
-// export const BACKEND_BASE_URL = 'https://api.auxo.fund';
-export const BACKEND_BASE_URL = 'https://api-dev.auxo.fund';
+export const BACKEND_BASE_URL = 'https://api.auxo.fund';
+// export const BACKEND_BASE_URL = 'https://api-dev.auxo.fund';
